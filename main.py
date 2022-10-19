ab1da4ddc62149a7ae164e7c7e4867aa1784d547
Hakuchi <71324797+hakuchi-1864@users.noreply.github.com>
2022-10-19 18:43:06 +0000

    add start-maximed arg. to maximize web browser

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
 from selenium import webdriver
 # allow us to instruct the behaviour of the web browser
+
 
 def get_driver():
   # Set options to make browsing easier
   options = webdriver.ChromeOptions()
   options.add_argument("disable-infobars")
-  options.add_argument("start-maximized")
+  options.add_argument("start-maximized")  #maximed web browser
   options.add_argument("disable-dev-shm-usage")
   options.add_argument("no-sandbox")
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -16,10 +16,11 @@ def get_driver():
   driver.get("http://py4e-data.dr-chuck.net/regex_sum_42.txt")
   return driver
 
+
 def main():
   driver = get_driver()
   element = driver.find_element(by="xpath", value="/html/body/pre")
   return element.text
 
-print(main())
 
+print(main())
